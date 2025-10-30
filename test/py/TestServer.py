d15bc4dbfb6bc4f2b82d1229339679ecc5b483ad
Carel Combrink <carel.combrink@gmail.com>
2025-10-30 06:44:21 +0000

    Starting to add UUID support for Python

--- test/py/TestServer.py
+++ test/py/TestServer.py
@@ -77,6 +77,11 @@ class TestHandler(object):
             logging.info('testBinary()')  # TODO: hex output
         return thing
 
+    def testUuid(self, thing):
+        if options.verbose > 1:
+            logging.info('testUuid(%s)' % thing)
+        return thing
+
     def testStruct(self, thing):
         if options.verbose > 1:
             logging.info('testStruct({%s, %s, %s, %s})' % (thing.string_thing, thing.byte_thing, thing.i32_thing, thing.i64_thing))
